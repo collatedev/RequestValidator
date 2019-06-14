0ec8fc3a1e509b4aa3796e91ba667c8c8fabdc37
Evan Coulson <evan.m.coulson@gmail.com>
2019-06-14 10:35:23 -0700

    added test for creating a type with a field

--- tests/ValidationSchema/TypeConfiguration.spec.ts
+++ tests/ValidationSchema/TypeConfiguration.spec.ts
@@ -1,6 +1,7 @@
 import ITypeConfiguration from "../../src/ValidationSchema/ITypeConfiguration";
 import TypeConfiguration from "../../src/ValidationSchema/TypeConfiguration";
 import IllegalSchemaError from "../../src/ValidationSchema/IllegalSchemaError";
+import IFieldConfiguration from "../../src/ValidationSchema/IFieldConfiguration";
 
 test('Throws an error creating an illegal type', () => {
     expect(createType(null)).toThrow(IllegalSchemaError);
@@ -11,6 +12,23 @@ test('Creates an empty type', () => {
 
     expect(type.getFields()).toHaveLength(0);
 });
+
+test('Creates a type with a field', () => {
+    const json : any = {
+        foo: {
+            required: true,
+            type: "string"
+        }
+    };
+
+    const type : ITypeConfiguration = new TypeConfiguration(json);
+    const fooField : IFieldConfiguration = type.getConfiguration("foo");
+
+    expect(type.getFields()).toHaveLength(1);
+    expect(fooField).not.toBeNull();
+    expect(fooField.required).toBeTruthy();
+    expect(fooField.type).toEqual("string");
+})
 
 function createType(json : any) : () => ITypeConfiguration {
     return (() : ITypeConfiguration => {
