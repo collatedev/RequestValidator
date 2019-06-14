047197c0f30f4d353853ed090e9859695c0c18dd
Evan Coulson <evan.m.coulson@gmail.com>
2019-06-14 10:28:23 -0700

    added test for creating empty type definitions

--- tests/ValidationSchema/TypeConfiguration.spec.ts
+++ tests/ValidationSchema/TypeConfiguration.spec.ts
@@ -6,6 +6,12 @@ test('Throws an error creating an illegal type', () => {
     expect(createType(null)).toThrow(IllegalSchemaError);
 });
 
+test('Creates an empty type', () => {
+    const type : ITypeConfiguration = new TypeConfiguration({});
+
+    expect(type.getFields()).toHaveLength(0);
+});
+
 function createType(json : any) : () => ITypeConfiguration {
     return (() : ITypeConfiguration => {
         return new TypeConfiguration(json);
