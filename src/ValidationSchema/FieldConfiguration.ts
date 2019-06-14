a106cbb01544fa1734f718f0d70eefca1cdfd056
Evan Coulson <evan.m.coulson@gmail.com>
2019-06-14 15:54:03 -0700

    implemented test for incorrect type of required

--- src/ValidationSchema/FieldConfiguration.ts
+++ src/ValidationSchema/FieldConfiguration.ts
@@ -27,7 +27,10 @@ export default class FieldConfiguration implements IFieldConfiguration {
             throw new IllegalSchemaError('The json configuration must have the "required" key');
         }
         if (typeof field.type !== 'string') {
-            throw new IllegalSchemaError('The type key must be a string');
+            throw new IllegalSchemaError('The key "type" must be a string');
+        }
+        if (typeof field.required !== 'boolean') {
+            throw new IllegalSchemaError('The key "required" must be a boolean');
         }
         this.required = field.required;
         this.type = field.type;
