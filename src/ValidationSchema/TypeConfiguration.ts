4f39b2afd0fc4c8873698ad25a2076ec879a7ca4
Evan Coulson <evan.m.coulson@gmail.com>
2019-06-14 10:27:17 -0700

    implemented test for null json in type configuration

--- src/ValidationSchema/TypeConfiguration.ts
+++ src/ValidationSchema/TypeConfiguration.ts
@@ -1,8 +1,12 @@
 import ITypeConfiguration from "./ITypeConfiguration";
 import IFieldConfiguration from "./IFieldConfiguration";
+import IllegalSchemaError from "./IllegalSchemaError";
 
 export default class TypeConfiguration implements ITypeConfiguration {
     constructor(type : any) {
+        if (type === null) {
+            throw new IllegalSchemaError("The json configuration of a type must not be null");
+        }
         // find fields of the type here
     }
 
