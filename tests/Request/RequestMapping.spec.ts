edf50c23714b7d61bc4f0bb0450ec831a4be5f29
Evan Coulson <evan.m.coulson@gmail.com>
2019-06-15 01:56:12 -0700

    added test for creating an empty request mapping

--- tests/Request/RequestMapping.spec.ts
+++ tests/Request/RequestMapping.spec.ts
@@ -9,6 +9,12 @@ test('should fail to create request mapping due to illegal type mapping', () => 
     expect(createMapping(1)).toThrow(TypeError);
 });
 
+test('should create an empty mapping', () => {
+    const mapping : IRequestMapping = new RequestMapping({});
+
+    expect(mapping.keys()).toHaveLength(0);
+});
+
 function createMapping(mapping : any) : () => IRequestMapping {
     return () : IRequestMapping => {
         return new RequestMapping(mapping);
