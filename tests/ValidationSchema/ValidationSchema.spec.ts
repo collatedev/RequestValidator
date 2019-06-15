eddbe3b0af8cdf5e7786690b44574ee8c05ce99f
Evan Coulson <evan.m.coulson@gmail.com>
2019-06-14 18:07:06 -0700

    added test to ensure that non-empty type schemas are validated

--- tests/ValidationSchema/ValidationSchema.spec.ts
+++ tests/ValidationSchema/ValidationSchema.spec.ts
@@ -2,6 +2,7 @@ import ValidationSchema from "../../src/ValidationSchema/ValidationSchema";
 import IllegalSchemaError from "../../src/ValidationSchema/IllegalSchemaError";
 import IValidationSchema from "../../src/ValidationSchema/IValidationSchema";
 import ITypeConfiguration from "../../src/ValidationSchema/ITypeConfiguration";
+import IFieldConfiguration from "../../src/ValidationSchema/IFieldConfiguration";
 
 test('Fails to create a validation schema due to null json', () => {
     expect(createValidationSchema(null)).toThrow(IllegalSchemaError);
@@ -45,6 +46,32 @@ test('Creates a validation schema with an empty type', () => {
     expect(schema.hasType("body")).toBeTruthy();
     expect(bodyType).not.toBeNull();
     expect(bodyType.getFields()).toHaveLength(0);
+});
+
+test('Creates a validation schema with a non-empty type', () => {
+    const json : any = {
+        types: {
+            body: {
+                foo: {
+                    required: true,
+                    type: "string"
+                }
+            }
+        }
+    };
+
+    const schema : IValidationSchema = new ValidationSchema(json);
+    const bodyType : ITypeConfiguration = schema.getTypeConfiguration("body");
+    const fooField : IFieldConfiguration = bodyType.getConfiguration("foo");
+
+    expect(schema).not.toBeNull();
+    expect(schema.getTypes()).toHaveLength(1);
+    expect(schema.hasType("body")).toBeTruthy();
+    expect(bodyType).not.toBeNull();
+    expect(bodyType.getFields()).toHaveLength(1);
+    expect(bodyType.getFields()[0]).toEqual("foo");
+    expect(fooField.type).toEqual("string");
+    expect(fooField.required).toBeTruthy();
 });
 
 function createValidationSchema(json : any) : () => IValidationSchema {
