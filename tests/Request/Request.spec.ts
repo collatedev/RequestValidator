e514b43a4ef6242b7f33fe4a77f1e86a9fc33068
Evan Coulson <evan.m.coulson@gmail.com>
2019-06-15 01:04:58 -0700

    added test cases for the constructor of request

--- tests/Request/Request.spec.ts
+++ tests/Request/Request.spec.ts
@@ -11,22 +11,6 @@ test('Creates an empty request', () => {
     testRequest(EmptyMapping, EmptyMapping, EmptyMapping, EmptyMapping, EmptyMapping);
 });
 
-function testRequest(
-    body : IRequestMapping, 
-    cookies : IRequestMapping,
-    headers : IRequestMapping, 
-    params : IRequestMapping, 
-    query : IRequestMapping
-) {
-    const request : IRequest = new Request(body, cookies, headers, params, query);
-
-    expect(request.getBody()).toEqual(body);
-    expect(request.getCookies()).toEqual(cookies);
-    expect(request.getHeaders()).toEqual(headers);
-    expect(request.getParams()).toEqual(params);
-    expect(request.getQuery()).toEqual(query);
-}
-
 test('Creates a request with a body', () => {
     testRequest(TestMapping, EmptyMapping, EmptyMapping, EmptyMapping, EmptyMapping);
 });
@@ -35,3 +19,30 @@ test('Creates a request with cookies', () => {
     testRequest(EmptyMapping, TestMapping, EmptyMapping, EmptyMapping, EmptyMapping);
 });
 
+test('Creates a request with headers', () => {
+    testRequest(EmptyMapping, EmptyMapping, TestMapping, EmptyMapping, EmptyMapping);
+});
+
+test('Creates a request with params', () => {
+    testRequest(EmptyMapping, EmptyMapping, EmptyMapping, TestMapping, EmptyMapping);
+});
+
+test('Creates a request with query', () => {
+    testRequest(EmptyMapping, EmptyMapping, EmptyMapping, EmptyMapping, TestMapping);
+});
+
+function testRequest(
+    body : IRequestMapping, 
+    cookies : IRequestMapping,
+    headers : IRequestMapping, 
+    params : IRequestMapping, 
+    query : IRequestMapping
+) : void {
+    const request : IRequest = new Request(body, cookies, headers, params, query);
+
+    expect(request.getBody()).toEqual(body);
+    expect(request.getCookies()).toEqual(cookies);
+    expect(request.getHeaders()).toEqual(headers);
+    expect(request.getParams()).toEqual(params);
+    expect(request.getQuery()).toEqual(query);
+}
