5f40dc30b56cc72ad392bd1be33dd30fc8857802
Evan Coulson <evan.m.coulson@gmail.com>
2019-06-15 01:03:54 -0700

    refactored test cases of request to be a single function

--- src/Request/Request.ts
+++ src/Request/Request.ts
@@ -2,40 +2,44 @@ import IRequest from "./IRequest";
 import IRequestMapping from "./IRequestMapping";
 
 export default class Request implements IRequest {
-    private body : IRequestMapping;
-    private cookies : IRequestMapping;
+    private readonly body : IRequestMapping;
+    private readonly cookies : IRequestMapping;
+    private readonly headers : IRequestMapping;
+    private readonly params : IRequestMapping;
+    private readonly query : IRequestMapping;
 
-    constructor() {
-        this.body = {};
-        this.cookies = {};
+    constructor(
+        body : IRequestMapping, 
+        cookies : IRequestMapping, 
+        headers : IRequestMapping, 
+        params : IRequestMapping, 
+        query : IRequestMapping
+    ) {
+        this.body = body;
+        this.cookies = cookies;
+        this.headers = headers;
+        this.params = params;
+        this.query = query;
     }
 
     public getBody(): IRequestMapping {
         return this.body;
     }
     
-    public setBody(body: IRequestMapping) : void {
-        this.body = body;
-    }
-    
     public getCookies(): IRequestMapping {
         return this.cookies;
     }
-
-    public setCookies(cookies : IRequestMapping) : void {
-        this.cookies = cookies;
-    }
     
     public getHeaders(): IRequestMapping {
-        return {};
+        return this.headers;
     }
     
     public getParams(): IRequestMapping {
-        return {};
+        return this.params;
     }
     
     public getQuery(): IRequestMapping {
-        return {};
+        return this.query;
     }
 
 
--- tests/Request/Request.spec.ts
+++ tests/Request/Request.spec.ts
@@ -1,45 +1,37 @@
 import Request from "../../src/Request/Request";
 import IRequest from "../../src/Request/IRequest";
+import IRequestMapping from "../../src/Request/IRequestMapping";
+
+const EmptyMapping : IRequestMapping = {};
+const TestMapping : IRequestMapping = {
+    foo: "bar"
+};
 
 test('Creates an empty request', () => {
-    const request : IRequest = new Request();
-
-    expect(request.getBody()).toEqual({});
-    expect(request.getCookies()).toEqual({});
-    expect(request.getHeaders()).toEqual({});
-    expect(request.getParams()).toEqual({});
-    expect(request.getQuery()).toEqual({});
+    testRequest(EmptyMapping, EmptyMapping, EmptyMapping, EmptyMapping, EmptyMapping);
 });
 
+function testRequest(
+    body : IRequestMapping, 
+    cookies : IRequestMapping,
+    headers : IRequestMapping, 
+    params : IRequestMapping, 
+    query : IRequestMapping
+) {
+    const request : IRequest = new Request(body, cookies, headers, params, query);
+
+    expect(request.getBody()).toEqual(body);
+    expect(request.getCookies()).toEqual(cookies);
+    expect(request.getHeaders()).toEqual(headers);
+    expect(request.getParams()).toEqual(params);
+    expect(request.getQuery()).toEqual(query);
+}
+
 test('Creates a request with a body', () => {
-    const request : Request = new Request();
-
-    request.setBody({
-        foo: 1
-    });
-
-    expect(request.getBody()).toEqual({
-        foo: 1
-    });
-    expect(request.getCookies()).toEqual({});
-    expect(request.getHeaders()).toEqual({});
-    expect(request.getParams()).toEqual({});
-    expect(request.getQuery()).toEqual({});
+    testRequest(TestMapping, EmptyMapping, EmptyMapping, EmptyMapping, EmptyMapping);
 });
 
 test('Creates a request with cookies', () => {
-    const request : Request = new Request();
-
-    request.setCookies({
-        foo: 1
-    });
-
-    expect(request.getBody()).toEqual({});
-    expect(request.getCookies()).toEqual({
-        foo: 1
-    });
-    expect(request.getHeaders()).toEqual({});
-    expect(request.getParams()).toEqual({});
-    expect(request.getQuery()).toEqual({});
+    testRequest(EmptyMapping, TestMapping, EmptyMapping, EmptyMapping, EmptyMapping);
 });
 
