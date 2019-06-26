ab9a2808681850c46b41cabdd1bc112cc4b41cba
Evan Coulson <evan.m.coulson@gmail.com>
2019-06-26 00:25:21 -0700

    added sanitization for strings

--- src/Sanitizer/Sanitizer.ts
+++ src/Sanitizer/Sanitizer.ts
@@ -5,18 +5,77 @@ import IValidationResult from "../ValidationResult/IValidationResult";
 import ValidationResult from "../ValidationResult/ValidationResult";
 import IErrorHandler from "../ErrorHandler/IErrorHandler";
 import IPathBuilder from "../PathBuilder/IPathBuilder";
-import ValidatorErrorHandler from "../ErrorHandler/ValidatorErrorHandler";
+import SanitizerErrorHandler from "../ErrorHandler/SanitizerErrorHandler";
+import IFieldConfiguration from "../ValidationSchema/IFieldConfiguration";
+import ErrorType from "../ErrorHandler/ErrorType";
+
+const urlRegexPattern : string = 
+'^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]' +
+'\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\' +
+'d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00' +
+'a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localho' +
+'st)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+
+const urlRegex : RegExp = new RegExp(urlRegexPattern, 'i');
 
 export default class Santizer implements ISanitizer {
     private result : IValidationResult;
     private errorHandler : IErrorHandler;
+    private pathBuilder : IPathBuilder;
 
     constructor(pathBuilder : IPathBuilder) {
-        this.errorHandler = new ValidatorErrorHandler(pathBuilder);
+        this.pathBuilder = pathBuilder;
+        this.errorHandler = new SanitizerErrorHandler(this.pathBuilder);
         this.result = new ValidationResult(this.errorHandler);
     }
 
     public sanitize(mapping : IRequestMapping, type : ITypeConfiguration) : IValidationResult {
+        this.errorHandler = new SanitizerErrorHandler(this.pathBuilder);
+        this.result = new ValidationResult(this.errorHandler);
+
+        for (const field of mapping.keys()) {
+            if (type.hasField(field)) {
+                const fieldConfiguration : IFieldConfiguration = type.getConfiguration(field);
+                const value : any = mapping.value(field);
+
+                if (fieldConfiguration.type === "string") {
+                    this.sanitizeString(field, value, fieldConfiguration);
+                }
+                
+            }
+        }
+
         return this.result;
+    }
+
+    private sanitizeString(field: string, value : any, configuration : IFieldConfiguration) : void {
+        if (this.isIllegalLength(value, configuration)) {
+            this.sanitizeIllegalLength(field, value, configuration);
+        }
+        if (this.doesNotStartWith(value, configuration)) {
+            this.errorHandler.handleError([value, configuration.startsWith], ErrorType.DoesNotStartWithError);
+        }
+        if (this.isIllegalURL(value, configuration)) {
+            this.errorHandler.handleError([value], ErrorType.IllegalURLError);
+        }
+    }
+
+    private isIllegalLength(value : any, configuration : IFieldConfiguration) : boolean {
+        return configuration.length !== undefined && value.length !== configuration.length;
+    }
+
+    private sanitizeIllegalLength(field : string , value : any, configuration : IFieldConfiguration) : void {
+        this.errorHandler.handleError(
+            [field, value.length, configuration.length], 
+            ErrorType.IllegalLengthError
+        );
+    }
+
+    private doesNotStartWith(value : any, configuration : IFieldConfiguration) : boolean {
+        return configuration.startsWith !== undefined && !value.startsWith(configuration.startsWith);
+    }
+
+    private isIllegalURL(value : any, configuration : IFieldConfiguration) : boolean {
+        return configuration.isURL !== undefined && !urlRegex.test(value);
     }
 }
--- tests/Sanitizer/Sanitizer.spec.ts
+++ tests/Sanitizer/Sanitizer.spec.ts
@@ -1,3 +1,117 @@
-test("Creates a sanitizer", () => {
-    expect(true).toBeTruthy();
+import Santizer from "../../src/Sanitizer/Sanitizer";
+import PathBuilder from "../../src/PathBuilder/PathBuilder";
+import IRequestMapping from "../../src/Request/IRequestMapping";
+import RequestMapping from "../../src/Request/RequestMapping";
+import ITypeConfiguration from "../../src/ValidationSchema/ITypeConfiguration";
+import TypeConfiguration from "../../src/ValidationSchema/TypeConfiguration";
+import IValidationResult from "../../src/ValidationResult/IValidationResult";
+import ISanitizer from "../../src/Sanitizer/ISanitizer";
+
+test("Sanitizes a string with valid length", () => {
+    const sanitizer : ISanitizer = new Santizer(new PathBuilder());
+    const mapping : IRequestMapping = new RequestMapping({
+        foo: "A"
+    });
+    const type : ITypeConfiguration = new TypeConfiguration({
+        foo: {
+            type: "string",
+            required: true,
+            length: 1
+        }
+    });
+
+    assertValidResult(sanitizer.sanitize(mapping, type));
 });
+
+test("Sanitizes a string with invalid length", () => {
+    const sanitizer : ISanitizer = new Santizer(new PathBuilder());
+    const mapping : IRequestMapping = new RequestMapping({
+        foo: "A"
+    });
+    const type : ITypeConfiguration = new TypeConfiguration({
+        foo: {
+            type: "string",
+            required: true,
+            length: 0
+        }
+    });
+
+    assertResultHasError(sanitizer.sanitize(mapping, type), "", "Length of 'foo' is 1 when it should be 0");
+});
+
+test("Sanitizes a string that does not start with foo", () => {
+    const sanitizer : ISanitizer = new Santizer(new PathBuilder());
+    const mapping : IRequestMapping = new RequestMapping({
+        foo: "A"
+    });
+    const type : ITypeConfiguration = new TypeConfiguration({
+        foo: {
+            type: "string",
+            required: true,
+            startsWith: "foo"
+        }
+    });
+
+    assertResultHasError(sanitizer.sanitize(mapping, type), "", "Value 'A' does not start with 'foo'");
+});
+
+test("Sanitizes a string that does start with foo", () => {
+    const sanitizer : ISanitizer = new Santizer(new PathBuilder());
+    const mapping : IRequestMapping = new RequestMapping({
+        foo: "foo"
+    });
+    const type : ITypeConfiguration = new TypeConfiguration({
+        foo: {
+            type: "string",
+            required: true,
+            startsWith: "foo"
+        }
+    });
+
+    assertValidResult(sanitizer.sanitize(mapping, type));
+});
+
+test("Sanitizes a string that is not a url", () => {
+    const sanitizer : ISanitizer = new Santizer(new PathBuilder());
+    const mapping : IRequestMapping = new RequestMapping({
+        foo: "A"
+    });
+    const type : ITypeConfiguration = new TypeConfiguration({
+        foo: {
+            type: "string",
+            required: true,
+            isURL: true
+        }
+    });
+
+    assertResultHasError(sanitizer.sanitize(mapping, type), "", "Value 'A' is not a valid URL");
+});
+
+test("Sanitizes a string that is a url", () => {
+    const sanitizer : ISanitizer = new Santizer(new PathBuilder());
+    const mapping : IRequestMapping = new RequestMapping({
+        foo: "http://res.cloudinary.com/hrscywv4p/image/upload/c_fill,g_faces:" +
+                "center,h_128,w_128/yflwk7vffgwyyenftkr7.png"
+    });
+    const type : ITypeConfiguration = new TypeConfiguration({
+        foo: {
+            type: "string",
+            required: true,
+            isURL: true
+        }
+    });
+
+    assertValidResult(sanitizer.sanitize(mapping, type));
+});
+
+function assertValidResult(result : IValidationResult) : void {
+    expect(result.isValid()).toBeTruthy();
+    expect(result.errors()).toHaveLength(0);
+}
+
+function assertResultHasError(result : IValidationResult, location: string, message : string) : void {
+    expect(result.isValid()).toBeFalsy();
+    expect(result.errors()).toHaveLength(1);
+    expect(result.errors()[0].location).toEqual(location);
+    expect(result.errors()[0].message).toEqual(message);
+}
