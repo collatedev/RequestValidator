5fa66a9fd8bcc7c9c23c78310e4a6b91e462bc85
Evan Coulson <evan.m.coulson@gmail.com>
2019-07-14 18:51:13 -0700

    refactored type checking

--- src/Types/TypeChecker.ts
+++ src/Types/TypeChecker.ts
@@ -8,6 +8,13 @@ import IsType from "./IsType";
 import ValidationResult from "../ValidationResult/ValidationResult";
 import ValidatorErrorHandler from "../ErrorHandler/ValidatorErrorHandler";
 import ErrorType from "../ErrorHandler/ErrorType";
+import ArrayType from "./ArrayType";
+import IndexPathComponent from "../PathBuilder/IndexPathComponent";
+import PropertyPathComponent from "../PathBuilder/PropertyPathComponent";
+import ITypeConfiguration from "../ValidationSchema/ITypeConfiguration";
+import INestedArray from "../NestedArray/INestedArray";
+import IPathComponent from "../PathBuilder/IPathComponent";
+import NestedArray from "../NestedArray/NestedArray";
 
 export default class TypeChecker implements ITypeChecker {
     private readonly schema : IValidationSchema;
@@ -27,16 +34,92 @@ export default class TypeChecker implements ITypeChecker {
         this.errorHandler = new ValidatorErrorHandler(this.pathBuilder);
         this.validationResult = new ValidationResult(this.errorHandler);
 
+        this.typeCheckValue(fieldName, value, configuration);
+
+        return this.validationResult;
+    }
+
+    public typeCheckValue(fieldName : string, value : any, configuration : IFieldConfiguration) : void {
         if (IsType.isPrimative(configuration.type) && !IsType.isTypeOf(configuration.type, value)) {
             this.errorHandler.handleError([fieldName, configuration.type], ErrorType.IncorrectType);
         } else if (IsType.isEnum(configuration.type) && !IsType.isTypeOf("string", value)) {
             this.errorHandler.handleError([fieldName, configuration.type], ErrorType.IncorrectType);
         }  else if (IsType.isArray(configuration.type)) {
-            // handle arrays
+            this.typeCheckArray(fieldName, value, configuration);
         } else if (this.schema.hasType(configuration.type) && !IsType.isNestedObject(value)) {
             this.errorHandler.handleError([fieldName, configuration.type], ErrorType.IncorrectType);
         } 
+    }
 
-        return this.validationResult;
+    private typeCheckArray(fieldName : string, value : any, configuration : IFieldConfiguration) : void {
+        if (!Array.isArray(value)) {
+            this.errorHandler.handleError([fieldName, configuration.type], ErrorType.IncorrectType);
+        } else {
+            const nestedArrayStack : INestedArray[] = [new NestedArray(value, 0, this.pathBuilder)];
+            while (nestedArrayStack.length > 0) {
+                const nestedArray : INestedArray = nestedArrayStack.pop() as INestedArray;
+                const array : any[] = nestedArray.value();
+                const arrayPath : IPathComponent[] = nestedArray.path().getCurrentIndexComponents();
+                this.pathBuilder.addPathComponents(arrayPath);
+    
+                this.enqueNestedArrays(array, nestedArrayStack, nestedArray);
+                this.typeCheckArrayElement(fieldName, nestedArray, configuration);
+
+                this.pathBuilder.popComponents(arrayPath.length);
+            }
+        }
+    }
+
+    private enqueNestedArrays(array : any[], nestedArrayStack : INestedArray[], nestedArray : INestedArray) : void {
+        for (let i : number = 0; i < array.length; i++) {
+            const nestedValue : any = array[i];
+            if (Array.isArray(nestedValue)) {
+                this.pathBuilder.addPathComponent(new IndexPathComponent(i));
+                nestedArrayStack.push(
+                    new NestedArray(
+                        nestedValue, 
+                        nestedArray.depth() + 1, 
+                        this.pathBuilder
+                    )
+                );
+                this.pathBuilder.popComponent();
+            }
+        }
+    }
+
+    private typeCheckArrayElement(
+        fieldName : string, nestedArray : INestedArray, arrayConfiguration : IFieldConfiguration
+    ) : void {
+        const elementConfiguration : IFieldConfiguration = ArrayType.getElementType(arrayConfiguration);
+        const array : any[] = nestedArray.value();
+        for (let i : number = 0; i < array.length; i++) {
+            const element : any = array[i];
+            
+            if (!Array.isArray(element)) {
+                this.pathBuilder.addPathComponent(new IndexPathComponent(i));
+
+                if (this.schema.hasType(elementConfiguration.type)) {
+                    if (IsType.isNestedObject(element)) {
+                        this.typeCheckNestedObjectElement(element, elementConfiguration);
+                    } else {
+                        this.errorHandler.handleError([fieldName, elementConfiguration.type], ErrorType.IncorrectType);
+                    }                    
+                } else {
+                    this.typeCheckValue(fieldName, element, elementConfiguration);
+                }
+
+                this.pathBuilder.popComponent();
+            }
+        }
+    }
+
+    private typeCheckNestedObjectElement(value : any, configuration : IFieldConfiguration) : void {
+        const typeConfiguration : ITypeConfiguration = this.schema.getTypeConfiguration(configuration.type);
+        for (const nestedField of Object.keys(value)) {
+            const fieldConfiguration : IFieldConfiguration = typeConfiguration.getConfiguration(nestedField);
+            this.pathBuilder.addPathComponent(new PropertyPathComponent(nestedField));
+            this.typeCheckValue(nestedField, value[nestedField], fieldConfiguration);
+            this.pathBuilder.popComponent();
+        }
     }
 }
--- tests/TypeChecker/TypeChecker.spec.ts
+++ tests/TypeChecker/TypeChecker.spec.ts
@@ -154,7 +154,208 @@ test("That the type is not a nested object", () => {
     );
 });
 
+test("That the type is an array", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), EmptySchema);
 
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [], new FieldConfiguration({
+        type: "array[string]",
+        required: true,
+    }));
+
+    expectValidResult(validationResult);
+});
+
+
+test("That the type is not an array", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), EmptySchema);
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", "string", new FieldConfiguration({
+        type: "array[string]",
+        required: true,
+    }));
+
+    expectInvalidResult(validationResult, "", "Property 'foo' should be type 'array[string]'");
+});
+
+test("That the element types of the array are strings", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), EmptySchema);
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", ["string"], new FieldConfiguration({
+        type: "array[string]",
+        required: true,
+    }));
+
+    expectValidResult(validationResult);
+});
+
+test("That the element types of the array are not strings", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), EmptySchema);
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [1], new FieldConfiguration({
+        type: "array[string]",
+        required: true,
+    }));
+
+    expectInvalidResult(validationResult, "[0]", "Property 'foo[0]' should be type 'string'");
+});
+
+test("That the element types of the array are booleans", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), EmptySchema);
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [true], new FieldConfiguration({
+        type: "array[boolean]",
+        required: true,
+    }));
+
+    expectValidResult(validationResult);
+});
+
+test("That the element types of the array are not booleans", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), EmptySchema);
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [1], new FieldConfiguration({
+        type: "array[boolean]",
+        required: true,
+    }));
+
+    expectInvalidResult(validationResult, "[0]", "Property 'foo[0]' should be type 'boolean'");
+});
+
+test("That the element types of the array are numbers", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), EmptySchema);
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [1], new FieldConfiguration({
+        type: "array[number]",
+        required: true,
+    }));
+
+    expectValidResult(validationResult);
+});
+
+test("That the element types of the array are not numbers", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), EmptySchema);
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [true], new FieldConfiguration({
+        type: "array[number]",
+        required: true,
+    }));
+
+    expectInvalidResult(validationResult, "[0]", "Property 'foo[0]' should be type 'number'");
+});
+
+test("That the element types of the array are enums", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), EmptySchema);
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", ["A"], new FieldConfiguration({
+        type: "array[enum]",
+        required: true,
+        values: ["A"]
+    }));
+
+    expectValidResult(validationResult);
+});
+
+test("That the element types of the array are not enums", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), EmptySchema);
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [1], new FieldConfiguration({
+        type: "array[enum]",
+        required: true,
+        values: ["A"]
+    }));
+
+    expectInvalidResult(validationResult, "[0]", "Property 'foo[0]' should be type 'enum'");
+});
+
+test("That the element types of the array are nested objects", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), new ValidationSchema({
+        types: {
+            foo: {
+            }
+        }
+    }));
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [{}], new FieldConfiguration({
+        type: "array[foo]",
+        required: true,
+    }));
+
+    expectValidResult(validationResult);
+});
+
+test("That the element types of the array are not nested objects", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), new ValidationSchema({
+        types: {
+            foo: {
+            }
+        }
+    }));
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [1], new FieldConfiguration({
+        type: "array[foo]",
+        required: true,
+    }));
+
+    expectInvalidResult(validationResult, "[0]", "Property 'foo[0]' should be type 'foo'");
+});
+
+test("That the nested object fields of an array have correct types", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), new ValidationSchema({
+        types: {
+            foo: {
+                bar: {
+                    type: "string",
+                    required: true
+                }
+            }
+        }
+    }));
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [{
+        bar: "baz"
+    }], new FieldConfiguration({
+        type: "array[foo]",
+        required: true,
+    }));
+
+    expectValidResult(validationResult);
+});
+
+test("That the nested object fields of an array do not have correct types", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), new ValidationSchema({
+        types: {
+            foo: {
+                bar: {
+                    type: "string",
+                    required: true
+                }
+            }
+        }
+    }));
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [{
+        bar: true
+    }], new FieldConfiguration({
+        type: "array[foo]",
+        required: true,
+    }));
+
+    expectInvalidResult(validationResult, "[0].bar", "Property 'bar' should be type 'string'");
+});
+
+test("That the nested arrays do not have correct element types", () => {
+    const typeChecker : ITypeChecker = new TypeChecker(new PathBuilder(), EmptySchema);
+
+    const validationResult : IValidationResult = typeChecker.typeCheck("foo", [
+        ["1", "1"],
+        ["1", 0]
+    ], new FieldConfiguration({
+        type: "array[array[string]]",
+        required: true,
+    }));
+
+    expectInvalidResult(validationResult, "[1][1]", "Property 'foo[1][1]' should be type 'string'");
+});
 
 function expectValidResult(validationResult : IValidationResult) : void {
     expect(validationResult.isValid()).toBeTruthy();
